fef2bcae2465dbc64b65e20fd45000c51fa69cb8
M <lxtdht@gmail.com>
2025-11-28 11:33:29 -0500

    Added Fibonacci in Go

---
+++ fibonacci/go/fibonacci.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const N int = 10
+
+func main() {
+	var f = [N]int{}
+	f[0] = 0
+	f[1] = 1
+	f[2] = 1
+
+	for i := 3; i < N; i++ {
+		f[i] = f[i-1] + f[i-2]
+	}
+
+	for _, x := range f {
+		fmt.Println(x)
+	}
+}
