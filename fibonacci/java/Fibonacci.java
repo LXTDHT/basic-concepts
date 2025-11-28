039d0e77edb349f8901df076fe326fcc528ecbc5
M <lxtdht@gmail.com>
2025-11-28 11:22:17 -0500

    Updated Java Fibonacci - Added 'final' keyword to make 'N' a constant - Updated the output loop to be a for-each/ranged

--- fibonacci/java/Fibonacci.java
+++ fibonacci/java/Fibonacci.java
@@ -1,15 +1,15 @@
 public class Fibonacci {
-    public static int n = 10; // Set the number of iterations here.
+    public static final int N = 10; // Set the number of iterations here.
 
     public static void main(String[] args) {
-        int[] f = new int[n]; f[0] = 0; f[1] = 1; f[2] = 1;
+        int[] f = new int[N]; f[0] = 0; f[1] = 1; f[2] = 1;
 
-        for (int i = 3; i < n; ++i) {
+        for (int i = 3; i < N; ++i) {
             f[i] = f[i - 1] + f[i - 2];
         }
-
-        for (int i = 0; i < n; ++i) {
-            System.out.println(f[i]);
+        
+        for (int x : f) {
+            System.out.println(x);
         }
     }
 }
